afa044b3b198c9b57bd1c6ce5bdec3e544764f03
Adam Koltuniuk <adamkoltuniuk@gmail.com>
2022-03-07 19:31:46 -0500

    Made into Nuget package with basic data getter

---
+++ dynmapTrackingApi/DynmapTracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace dynmapTrackingApi
+{
+    public class DynmapTracker
+    {
+        public class Tracker{
+            public Tracker(string mapLink)
+            {
+                MapLink = mapLink;
+            }
+
+            public string MapLink { get; private set; }
+
+            public async System.Threading.Tasks.Task<JObject> GetRawDataAsync()
+            {
+                var httpClient = new HttpClient();
+                var data = httpClient.GetAsync(MapLink);
+                var bodyData = await data.Result.Content.ReadAsStringAsync();
+                return JObject.Parse(bodyData);
+            }
+        }
+        
+
+    }
+}
